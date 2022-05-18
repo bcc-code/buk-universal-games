72c1a0660b4ef43cf849fe26976f5cdffb6f7c88
Reng van Oord <reng.vanoord@bcc.no>
2022-05-18 11:08:08 +0200

    Added redis infrastructure

---
+++ infra/install.sh
@@ -0,0 +1,29 @@
+# Install Homebrew
+sudo apt update
+sudo apt-get install build-essential curl file git
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+
+
+# Install Terraform
+# Ref: https://learn.hashicorp.com/tutorials/terraform/install-cli
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+
+# Install Terragrunt using homebrew
+brew install terragrunt
+
+# Install Google CLI
+# Ref: https://cloud.google.com/sdk/docs/install#deb
+sudo apt-get install apt-transport-https ca-certificates gnupg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.gpg
+sudo apt-get update && sudo apt-get install google-cloud-cli
--- infra/src/services/cloud-run-api/outputs.tf
+++ infra/src/services/cloud-run-api/outputs.tf
@@ -6,3 +6,4 @@ output "service" {
 output "backend-service" {
   value = google_compute_backend_service.default
 }
+
