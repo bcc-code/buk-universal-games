5b095c52bb72c6639cf79d6948093402c2e60068
Ole Simon Evensen <ole@memosoft.no>
2022-06-11 23:00:31 +0200

    Add color to teams

---
+++ backend/Buk.UniversalGames.Data/Migrations/20220611205904_Add_Color_To_Teams.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Buk.UniversalGames.Api.Migrations
+{
+    public partial class Add_Color_To_Teams : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "color",
+                table: "teams",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "color",
+                table: "teams");
+        }
+    }
+}
--- backend/Buk.UniversalGames.Data/Models/Team.cs
+++ backend/Buk.UniversalGames.Data/Models/Team.cs
@@ -14,6 +14,8 @@ public class Team
 
     public string Code { get; set; }
 
+    public string Color { get; set; }
+
     public int? LeagueId { get; set; }
 
     [JsonIgnore]
