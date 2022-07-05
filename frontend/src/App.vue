0a7f38e93f43b18d07891277dd5555e961b15cec
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2022-07-05 22:50:51 +0200

    WIP - Login page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,50 @@
 <template>
-  <HomePage msg="Hello BUK"/>
+  <LoginPage />
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue'
+import LoginPage from './components/LoginPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HomePage
+    LoginPage
   }
 }
 </script>
 
 <style>
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  :root {
+    --dark: #2D001C;
+  }
+
+  html {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+  }
+
+  img {
+    width: 100%;
+    max-width: 100%;
+  }
+
+  button,
+  input {
+    border-radius: .5em;
+    padding: .75em;
+    text-align: center;
+    border: none;
+  }
+
+  .btn-primary {
+    background-color: var(--dark);
+    color: white;
+  }
 </style>
---
+++ frontend/src/components/LoginPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="bg" :style="{
+    'background-image': `url(${image})`,
+    }">
+    <img src="../assets/logo.png" alt="" class="logo">
+    <input type="text" placeholder="Team code" v-model="teamCode">
+    <button class="btn-primary" @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  data() {
+    return {
+        image: require("@/assets/bg.png"),
+        teamCode: null,
+    }
+  },
+  methods: {
+    login() {
+        console.log("teamcode", this.teamCode)
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .bg {
+        height: 100vh;
+        background-size: cover;
+        background-position: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 1em;
+    }
+
+    button {
+        margin: 1em 0;
+    }
+</style>
