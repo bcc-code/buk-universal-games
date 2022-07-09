08229f14f2bcb4a013a9bcf7e2fd3f9ebd42ce6c
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2022-07-10 01:41:27 +0200

    Show points and stickers

--- frontend/src/pages/LeagueList.vue
+++ frontend/src/pages/LeagueList.vue
@@ -1,27 +1,35 @@
 <template>
   <UserPageLayout>
-    hello
+    <PointsAndStickers :points="teamStatus?.status?.points" :stickers="teamStatus?.status?.stickers" :refresh="() => refreshStatus()"/>
   </UserPageLayout>
 </template>
 
 <script>
 import UserPageLayout from '../components/UserPageLayout.vue'
+import PointsAndStickers from '../components/PointsAndStickers.vue'
 
 export default {
   name: 'LoginPage',
   props: {
     data: String,
   },
-  components: { UserPageLayout },
+  components: { UserPageLayout, PointsAndStickers },
   data() {
-    return {
-        loginError: "Hello",
+  },
+  created() {
+    if(Object.keys(this.$store.state.teamStatus).length === 0) {
+       this.refreshStatus() 
     }
   },
-  mounted() {
-  },
   methods: {
-
+    refreshStatus() {
+      this.$store.dispatch("getTeamStatus")
+    }
+  },
+  computed: {
+    teamStatus() {
+      return this?.$store.state.teamStatus
+    }
   }
 }
 </script>
