b8f9fd7a37cbbd42f5c53dd13d15175a861a3448
Bjørnar Hagen <bjornar@datahjelpen.no>
2022-07-10 02:18:50 +0200

    Add GameInfo page

---
+++ frontend/src/assets/icons/game-fire.svg.ts
@@ -0,0 +1,67 @@
+export const gameFireIcon = `<svg width="29" height="33" viewBox="0 0 29 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g opacity="0.75">
+        <path d="M15.1155 15.389L0 7.69402L13.8845 0L29 7.69402L15.1155 15.389Z" fill="#FF334C" />
+        <path d="M0 25.306V7.69402L15.1155 15.389V33L0 25.306Z" fill="#FF8B35" />
+        <path d="M29 7.69402L15.1155 15.389V33L29 25.306V7.69402Z" fill="#801A26" />
+    </g>
+    <g style="mix-blend-mode:lighten" opacity="0.5">
+        <g style="mix-blend-mode:lighten">
+            <path d="M15.1174 6.10291L10.6312 3.81885L13.8826 2.01776L18.3698 4.30086L15.1174 6.10291Z" fill="#FF334C" />
+        </g>
+        <g style="mix-blend-mode:lighten">
+            <path d="M10.6312 16.6079V3.81885L15.1174 6.10291V18.891L10.6312 16.6079Z" fill="#FF8B35" />
+        </g>
+        <g style="mix-blend-mode:lighten">
+            <path d="M18.3698 4.30086L15.1174 6.10291V18.891L18.3698 17.0889V4.30086Z" fill="#801A26" />
+        </g>
+    </g>
+    <g style="mix-blend-mode:lighten" opacity="0.5">
+        <g style="mix-blend-mode:lighten">
+            <path d="M17.3605 16.0648L12.8743 13.7807L16.1257 11.9787L20.6129 14.2627L17.3605 16.0648Z" fill="#FF334C" />
+        </g>
+        <g style="mix-blend-mode:lighten">
+            <path d="M12.8743 26.5687V13.7807L17.3605 16.0648V28.8528L12.8743 26.5687Z" fill="#FF8B35" />
+        </g>
+        <g style="mix-blend-mode:lighten">
+            <path d="M20.6129 14.2627L17.3605 16.0648V28.8528L20.6129 27.0508V14.2627Z" fill="#801A26" />
+        </g>
+    </g>
+    <g style="mix-blend-mode:lighten" opacity="0.5">
+        <g style="mix-blend-mode:lighten">
+            <path d="M12.542 16.0648L8.05576 13.7807L11.3071 11.9787L15.7943 14.2627L12.542 16.0648Z" fill="#FF334C" />
+        </g>
+        <g style="mix-blend-mode:lighten">
+            <path d="M8.05576 26.5687V13.7807L12.542 16.0648V28.8528L8.05576 26.5687Z" fill="#FFB986" />
+        </g>
+        <g style="mix-blend-mode:lighten">
+            <path d="M15.7943 14.2627L12.542 16.0648V28.8528L15.7943 27.0508V14.2627Z" fill="#801A26" />
+        </g>
+    </g>
+    <g style="mix-blend-mode:screen">
+        <path d="M9.46054 25.8682V23.9058L11.1372 24.7591V26.7215L9.46054 25.8682Z" fill="#FFD6DB" />
+    </g>
+    <g style="mix-blend-mode:screen">
+        <path d="M13.2246 30.8582V29.5763L14.3197 30.1337V31.4156L13.2246 30.8582Z" fill="#FFD6DB" />
+    </g>
+    <g style="mix-blend-mode:screen" opacity="0.5">
+        <path d="M19.4252 18.0033L18.2168 18.6724V20.6243L19.4252 19.9542V18.0033Z" fill="#FF8B35" />
+    </g>
+    <g style="mix-blend-mode:screen">
+        <path d="M17.6239 25.1886L16.7563 25.6687V27.0699L17.6239 26.5888V25.1886Z" fill="#FF8B35" />
+    </g>
+    <path style="mix-blend-mode:color-dodge" d="M29 25.306V7.69402L15.1155 15.389L0 7.69402V25.306L15.1155 33L29 25.306Z" fill="url(#paint0_radial_122_3826)" />
+    <defs>
+        <radialGradient id="paint0_radial_122_3826" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(14.2734 25.8901) scale(22.2612 22.5065)">
+            <stop offset="0.08" stop-color="#FF8B35" />
+            <stop offset="0.09" stop-color="#FF8835" stop-opacity="0.97" />
+            <stop offset="0.2" stop-color="#FF743A" stop-opacity="0.74" />
+            <stop offset="0.31" stop-color="#FF623F" stop-opacity="0.54" />
+            <stop offset="0.42" stop-color="#FF5443" stop-opacity="0.38" />
+            <stop offset="0.53" stop-color="#FF4846" stop-opacity="0.24" />
+            <stop offset="0.64" stop-color="#FF3E48" stop-opacity="0.14" />
+            <stop offset="0.76" stop-color="#FF384A" stop-opacity="0.06" />
+            <stop offset="0.87" stop-color="#FF344B" stop-opacity="0.01" />
+            <stop offset="1" stop-color="#FF334C" stop-opacity="0" />
+        </radialGradient>
+    </defs>
+</svg>`;
--- frontend/src/pages/GameInfo.vue
+++ frontend/src/pages/GameInfo.vue
@@ -1,9 +1,48 @@
 <template>
-  <UserPageLayout> Game Info </UserPageLayout>
+  <UserPageLayout>
+    <header class="banner">Banner</header>
+
+    <h2>Games</h2>
+
+    <ul v-if="loading" class="games games-loading">
+      <li class="game">
+        <span class="game-icon" v-html="icons.Metal"></span>
+        <h3 class="game-title">Loading ...</h3>
+      </li>
+      <li class="game">
+        <span class="game-icon" v-html="icons.Metal"></span>
+        <h3 class="game-title">Loading ...</h3>
+      </li>
+      <li class="game">
+        <span class="game-icon" v-html="icons.Metal"></span>
+        <h3 class="game-title">Loading ...</h3>
+      </li>
+      <li class="game">
+        <span class="game-icon" v-html="icons.Metal"></span>
+        <h3 class="game-title">Loading ...</h3>
+      </li>
+      <li class="game">
+        <span class="game-icon" v-html="icons.Metal"></span>
+        <h3 class="game-title">Loading ...</h3>
+      </li>
+    </ul>
+    <ul v-else class="games">
+      <li class="game" v-for="game in games" :key="game.id">
+        <span class="game-icon" v-html="icons[game.name]"></span>
+        <h3 class="game-title">{{ game.name }}</h3>
+      </li>
+    </ul>
+  </UserPageLayout>
 </template>
 
 <script>
-import UserPageLayout from "../components/UserPageLayout.vue";
+import UserPageLayout from "@/components/UserPageLayout.vue";
+import { getData } from "@/libs/apiHelper";
+import { gameEarthIcon } from "@/assets/icons/game-earth.svg.ts";
+import { gameFireIcon } from "@/assets/icons/game-fire.svg.ts";
+import { gameMetalIcon } from "@/assets/icons/game-metal.svg.ts";
+import { gameWoodIcon } from "@/assets/icons/game-wood.svg.ts";
+import { gameWaterIcon } from "@/assets/icons/game-water.svg.ts";
 
 export default {
   name: "LoginPage",
@@ -14,11 +53,76 @@ export default {
   data() {
     return {
       loginError: "Game Info",
+      games: [],
+      loading: true,
+      icons: {
+        Earth: gameEarthIcon,
+        Fire: gameFireIcon,
+        Metal: gameMetalIcon,
+        Wood: gameWoodIcon,
+        Water: gameWaterIcon,
+      },
     };
   },
-  mounted() {},
+  mounted() {
+    getData("/Games")
+      .then((r) => r.json())
+      .then((r) => {
+        this.games = r;
+        this.loading = false;
+      })
+      .catch((e) => {
+        this.loading = false;
+        console.error(e);
+      });
+  },
   methods: {},
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.banner {
+  width: 100%;
+  padding: 2em;
+  border-radius: 1em;
+  margin: 2em auto;
+  background-color: var(--dark);
+  color: var(--green);
+}
+
+.games {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  gap: 1.5em;
+}
+
+.games .game {
+  color: #fff;
+  background-color: var(--dark);
+  border-radius: 1em;
+  padding: 1.5em;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+}
+
+.games-loading .game {
+  background-color: var(--gray-2);
+}
+
+.games .game .game-title {
+  margin: 1rem 0 0;
+  font-weight: 400;
+}
+
+.games .game .game-icon {
+  margin: 0;
+  width: 2em;
+  height: 2em;
+  overflow: hidden;
+}
+</style>
