c0c29d89fc56ee2c4f8b4a22776edee3a48deb25
Bjørnar Hagen <bjornar@datahjelpen.no>
2022-07-15 01:21:24 +0200

    fix apiHelper URL. formatting

--- frontend/src/libs/apiHelper.js
+++ frontend/src/libs/apiHelper.js
@@ -1,35 +1,27 @@
-// export  const rootUrl = "https://universalgames.buk.no/api/"
-export const rootUrl = "https://10.0.0.3:3000/api/"
-export function initData(url, data) {
-  // const rootUrl = "/api/"
+export const rootUrl = "https://universalgames.buk.no/api/"
 
+export function initData(url, data) {
   const teamCode = myGetTeamCodeFunction()
 
-  const response = fetch(rootUrl + url + teamCode, {
-    method: "GET", // *GET, POST, PUT, DELETE, etc.
+  return fetch(rootUrl + url + teamCode, {
+    method: "GET",
     headers: {
       "Accept": "application/json",
     },
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
+    body: JSON.stringify(data),
   })
-
-  return response; // parses JSON response into native JavaScript objects
 }
 
 export function getData(url, data) {
-  // const rootUrl = "/api/"
-
   const teamCode = myGetTeamCodeFunction()
 
-  const response = fetch(rootUrl + teamCode + url, {
-    method: "GET", // *GET, POST, PUT, DELETE, etc.
+  return fetch(rootUrl + teamCode + url, {
+    method: "GET",
     headers: {
       "Accept": "application/json",
     },
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
+    body: JSON.stringify(data),
   })
-
-  return response; // parses JSON response into native JavaScript objects
 }
 
 export function postStickerCode(stickerCode) {
@@ -52,6 +44,5 @@ export function postStickerCode(stickerCode) {
 }
 
 export function myGetTeamCodeFunction() {
-  // Get from local storage
   return window.localStorage.getItem('teamCode');
 }
