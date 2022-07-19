89eb3f89e407d7049bea0dc4fc6d8c86ca281c1a
Ole Simon Evensen <ole@memosoft.no>
2022-07-19 22:20:52 +0200

    Add ParticipantsInfo and SafetyInfo to Game

---
+++ backend/Buk.UniversalGames.Data/Migrations/20220719201909_Add_Participants_And_Safety_Info_To_Game.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Buk.UniversalGames.Api.Migrations
+{
+    public partial class Add_Participants_And_Safety_Info_To_Game : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "participants_info",
+                table: "games",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "safety_info",
+                table: "games",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "participants_info",
+                table: "games");
+
+            migrationBuilder.DropColumn(
+                name: "safety_info",
+                table: "games");
+        }
+    }
+}
