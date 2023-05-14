5d4b2e4284fa52c855a0a6f90da6ca42658fa5d8
Ghostbird <gijsbertth@gmail.com>
2023-05-14 15:16:46 +0200

    Fix HTTP in dev mode notation:
    Old notation was deprecated
    PWA doesn't work in Chrome with HTTPS, disable it to test.
    PWA doesn't work in Firefox Android without HTTPS.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    https: true
+    server: {
+      type: 'https'
+    }
   }
 })
