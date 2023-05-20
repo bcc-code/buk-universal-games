a72e2b29ddc06086b0fe4c9faba87ead8b63676c
Ruud Kobes <ruudkobes@gmail.com>
2023-05-20 20:17:30 +0200

    Also include migration for time zone change

---
+++ backend/Buk.UniversalGames.Data/Migrations/20230520180322_ChangeColumnTypeForMatchStart.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Buk.UniversalGames.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeColumnTypeForMatchStart : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "start",
+                table: "matches",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "start",
+                table: "matches",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+    }
+}
