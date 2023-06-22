a5b4aa7deefd0304073656c84a29534fd8f0c916
Ghostbird <gijsbertth@gmail.com>
2023-06-23 00:21:00 +0200

    Create and match notifier plugin and add to store

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,18 @@
 import { createApp, } from 'vue'
 import App from './App.vue'
-import router from '../src/router'
-import store from '../src/store'
+import initRouter from '../src/router'
+import initStore from '../src/store'
 import { setupI18n } from './libs/i18n'
-const store = initStore();
+import NotificationService from './services/notification.service'
+import createMatchNotifierPlugin from './plugins/match-notifier'
+
+const notificationService = new NotificationService();
+const matchNotifierPlugin = createMatchNotifierPlugin(notificationService);
+const store = initStore(matchNotifierPlugin);
 
 createApp(App)
     .use(store)
     .use(initRouter(store))
+    .use(setupI18n({ locale: 'no' }))
+    .provide('notificationService', notificationService)
     .mount('#app')
