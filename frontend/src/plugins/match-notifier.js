11449240704c4b42a60ec1a46b483f3e266b8338
Ghostbird <gijsbertth@gmail.com>
2023-07-23 15:03:38 +0200

    Ensure notifications are scheduled on the minute

--- frontend/src/plugins/match-notifier.js
+++ frontend/src/plugins/match-notifier.js
@@ -4,7 +4,7 @@ export default (notificationService, timeOffset = -300_000) => (store) => {
   store.subscribe(async (mutation, state) => {
     if (mutation.type === 'setMatches') {
       for (let match of mutation.payload) {
-        const time = new Date(new Date().setHours(...match.start.split(":")) + timeOffset)
+        const time = new Date(new Date().setHours(...match.start.split(":").concat([0, 0])) + timeOffset)
         const game = state.games.find(g => g.id === match.gameId);
         notificationService.schedule(time, `${match.start}: ${game.name}`, { body: `${match.team1} — ${match.team2}` })
       }
