de40fe1b2e62b239c2805d1fe8c8eda821c3ef16
Kristiane Bratlie <kristianebratlie@gmail.com>
2024-04-10 23:46:27 +0200

    Menu for naviagtion styling

--- frontend/src/components/UserMenu.vue
+++ frontend/src/components/UserMenu.vue
@@ -1,33 +1,49 @@
 <template>
-  <nav class="user-menu">
-    
-    <div class="user-menu-btn-small-wrapper">
-      <button :class="{ 'user-menu-btn-small': true, 'selected-component': isSelected('LeagueList') }"
-        @click="navigateTo('LeagueList')"><img src="/icon/home.svg" /></button>
-
-      <button :class="{ 'user-menu-btn-small': true, 'selected-component': isSelected('MatchList') }"
-        @click="navigateTo('MatchList')"><img src="/icon/calendar.svg" /></button>
-    </div>
-    <div class="user-menu-btn-big-wrapper">
-      <button class="user-menu-btn-big" @click="navigateTo('SideQuest')">
-        <img src="/icon/sidequest.svg" style="width:5em" />
+  <nav
+    class="flex w-full bottom-0 bg-ice-blue absolute align-middle p-2 justify-between"
+  >
+    <div class="flex w-1/2 justify-between align-middle">
+      <button
+        :class="{
+          'align-start': true,
+          'selected-component': isSelected('LeagueList'),
+        }"
+        @click="navigateTo('LeagueList')"
+      >
+        <img class="h-10 w-10" src="/icon/leaderlist.png" />
       </button>
     </div>
-    <div class="user-menu-btn-small-wrapper">
-      <button :class="{ 'user-menu-btn-small': true, 'selected-component': isSelected('GameInfo', 'GameInfoDetail') }"
-        @click="navigateTo('GameInfo')"><img src="/icon/ball.svg" /></button>
-      <button :class="{ 'user-menu-btn-small': true, 'selected-component': isSelected('Map') }"
-        @click="navigateTo('Map')"><img src="/icon/place.svg" /></button>
+    <div class="justify-center flex w-full">
+      <button
+        class="user-menu-btn-big bg-white"
+        @click="navigateTo('MatchList')"
+        :class="{
+          'selected-component': isSelected('MatchList'),
+        }"
+      >
+        <img src="/icon/match.png" style="width: 5em" />
+      </button>
+    </div>
+    <div class="flex w-1/2 justify-between">
+      <button
+        :class="{
+          'align-start': true,
+          'selected-component': isSelected('Map'),
+        }"
+        @click="navigateTo('Map')"
+      >
+        <img src="/icon/place.svg" />
+      </button>
     </div>
   </nav>
 </template>
 
 <script>
-import { defineComponent, reactive, toRefs } from 'vue'
-import { useRouter, useRoute } from 'vue-router'
+import { defineComponent, reactive, toRefs } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
 
 export default defineComponent({
-  name: "UserMenu",
+  name: 'UserMenu',
   setup() {
     const router = useRouter();
     const route = useRoute();
@@ -42,7 +58,9 @@ export default defineComponent({
     };
 
     const isSelected = (name, additionalName = null) => {
-      return state.selectedRoute === name || state.selectedRoute === additionalName;
+      return (
+        state.selectedRoute === name || state.selectedRoute === additionalName
+      );
     };
 
     return { ...toRefs(state), navigateTo, isSelected };
@@ -51,48 +69,13 @@ export default defineComponent({
 </script>
 
 <style scoped>
-input[type="file"] {
+input[type='file'] {
   display: none;
 }
 
-.user-menu {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 2em;
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  max-width: 960px;
-  margin: 0 auto;
-  background-color: transparent;
-  background-color: white;
-}
-
-button {
-  background-color: inherit;
-}
-
-.user-menu-btn-small-wrapper {
-  display: flex;
-  width: 50%;
-  justify-content: space-between;
-}
-
-.user-menu-btn-small {
-  align-self: flex-start;
-  padding: 0;
-}
-
-.user-menu-btn-small>img {
+.user-menu-btn-small > img {
   /* This trick allows us to colourise <img /> tag SVGs. */
-  filter: var(--inactive-yellow-button)
-}
-
-.user-menu-btn-big-wrapper {
-  width: 100%;
-  display: flex;
-  justify-content: center;
+  filter: var(--dark-brown);
 }
 
 .user-menu-btn-big {
@@ -100,17 +83,17 @@ button {
   height: 5em;
   width: 5em;
   top: -1.5em;
-  background-color: var(--dark);
+  background-color: var(--ice-blue);
   color: white;
   border-radius: 10em;
   display: flex;
-  padding: 3px;
+  padding: 10px;
   justify-content: center;
   align-items: center;
   border: 5px solid white;
 }
 
-.selected-component>img {
+.selected-component > img {
   /* This trick allows us to colourise <img /> tag SVGs. */
   filter: var(--active-green-button);
 }
