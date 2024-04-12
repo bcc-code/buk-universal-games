2915bf0c2898b3a980f692f8172b8243fa528b4d
Kristiane Bratlie <kristianebratlie@gmail.com>
2024-04-12 19:37:31 +0200

    Static position loginpage

--- frontend/src/pages/LoginPage.vue
+++ frontend/src/pages/LoginPage.vue
@@ -1,6 +1,9 @@
 <template>
-  <section class="px-5 flex justify-center items-center h-screen">
-    <form class="flex flex-col py-10 space-y-10 justify-center align-middle" @submit="tryLogin">
+  <section class="px-5 flex items-start pt-20 h-screen">
+    <form
+      class="flex flex-col py-10 space-y-10 justify-center align-middle"
+      @submit="tryLogin"
+    >
       <img src="/image/logo_icon.svg" alt="" class="logo" />
       <input
         type="text"
@@ -8,7 +11,10 @@
         :placeholder="$t('login.teamcode')"
         v-model="teamCode"
       />
-      <button v-if="teamCode.length > 3" class="bg-vanilla text-lg text-dark-brown py-3 px-2">
+      <button
+        class="bg-vanilla text-lg text-dark-brown py-3 px-2"
+        :class="[teamCode.length < 3 ? 'opacity-0' : 'opacity-100']"
+      >
         {{ $t('login.login_button') }}
       </button>
 
@@ -21,71 +27,71 @@
 </template>
 
 <script>
-import { inject } from 'vue'
+import { inject } from 'vue';
 export default {
   name: 'LoginPage',
   props: {
-    code: String
+    code: String,
   },
   data() {
     return {
       teamCode: '',
-      notificationService: null
-    }
+      notificationService: null,
+    };
   },
   mounted() {
-    this.$store.commit('setLoginMessage', '')
-    this.notificationService = inject('notificationService')
+    this.$store.commit('setLoginMessage', '');
+    this.notificationService = inject('notificationService');
     if (this.code) {
-      this.teamCode = this.code
-      this.tryLogin()
+      this.teamCode = this.code;
+      this.tryLogin();
     }
   },
   methods: {
     async tryLogin(ev) {
       // Do not perform normal HTML form submit.
       if (ev) {
-        ev.preventDefault()
+        ev.preventDefault();
       }
       try {
-        this.notificationService.requestExternal()
+        this.notificationService.requestExternal();
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
 
-      window.localStorage.setItem('testTeamCode', this.teamCode.toUpperCase())
-      const loginData = await this.$store.dispatch('signIn')
-      this.$store.commit('setLoginMessage', '')
+      window.localStorage.setItem('testTeamCode', this.teamCode.toUpperCase());
+      const loginData = await this.$store.dispatch('signIn');
+      this.$store.commit('setLoginMessage', '');
       if (!loginData || loginData.error) {
-        window.localStorage.removeItem('testTeamCode')
+        window.localStorage.removeItem('testTeamCode');
         if (loginData.error) {
-          this.$store.commit('setLoginMessage', loginData.error)
+          this.$store.commit('setLoginMessage', loginData.error);
         } else {
           this.$store.commit(
             'setLoginMessage',
-            'Something went wrong, we could not log you in. Please try again'
-          )
+            'Something went wrong, we could not log you in. Please try again',
+          );
         }
       } else {
-        await this.$store.dispatch('getGames')
+        await this.$store.dispatch('getGames');
 
         if (loginData.access.toLowerCase() === 'admin') {
-          await this.$store.dispatch('getAdminLeagues')
-          this.$store.dispatch('getAdminLeagueStatus')
-          this.$router.push({ name: 'AdminSelectLeague' })
+          await this.$store.dispatch('getAdminLeagues');
+          this.$store.dispatch('getAdminLeagueStatus');
+          this.$router.push({ name: 'AdminSelectLeague' });
         } else {
-          this.$store.dispatch('getMatches')
-          this.$router.push({ name: 'LeagueList' })
+          this.$store.dispatch('getMatches');
+          this.$router.push({ name: 'LeagueList' });
         }
       }
-    }
+    },
   },
   computed: {
     loginMessage() {
-      return this.$store.state.loginMessage ?? ''
-    }
-  }
-}
+      return this.$store.state.loginMessage ?? '';
+    },
+  },
+};
 </script>
 
 <style scoped>
