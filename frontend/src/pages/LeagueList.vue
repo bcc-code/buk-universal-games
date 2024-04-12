e8d6757e07fc86274535c36551a1ca44232263c4
Sigurd Schoeler <sigurd@finexa.no>
2024-04-12 23:23:22 +0200

    Show correct teamName

--- frontend/src/pages/LeagueList.vue
+++ frontend/src/pages/LeagueList.vue
@@ -144,7 +144,7 @@ export default {
       return this?.$store.state.leagueStatus;
     },
     teamName() {
-      return this?.$store.state.teamName;
+      return this?.$store.state.loginData.team;
     },
   },
 };
