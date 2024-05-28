1b2993311be6d05ee40b30cd53feddcb6df424ea
Kristiane Bratlie <kristianebratlie@gmail.com>
2024-05-28 23:11:14 +0200

    Game descriptions

--- frontend/src/pages/GameInfoDetail.vue
+++ frontend/src/pages/GameInfoDetail.vue
@@ -2,12 +2,18 @@
   <UserPageLayout :showTitle="false">
     <div class="flex">
       <nav class="text-dark mr-4">
-        <div @click="$router.back()" class="p-1 bg-ice-blue cursor-pointer rounded-md hover:bg-vanilla text-dark-blue">
+        <div
+          @click="$router.back()"
+          class="p-1 bg-ice-blue cursor-pointer rounded-md hover:bg-vanilla text-label-1"
+        >
           <ArrowLeftIcon class="h-5" />
         </div>
       </nav>
-      <h1 class="text-white font-bold py-2 px-2 rounded-md flex space-x-3">
-        <img class="h-10 w-10" :src="`/icon/game-${gameParsed.gameType.replaceAll('_', '')}.svg`" />
+      <h1 class="text-label-1 font-bold py-2 px-2 rounded-md flex space-x-3">
+        <img
+          class="h-10 w-10"
+          :src="`/icon/game-${gameParsed.gameType.replaceAll('_', '')}.svg`"
+        />
         <span>{{ $t(`games.${gameParsed.gameType}`) }}</span>
       </h1>
     </div>
@@ -19,41 +25,59 @@
         </video>
       </div> -->
     </div>
-    <div class="bg-ice-blue rounded-md p-4 ">
-      <p class="text-dark-blue">
-        {{ $t("leadstory." + gameParsed.gameType) }}
+    <div class="bg-ice-blue rounded-md p-4">
+      <p class="text-dark-blue whitespace-pre-wrap">
+        {{ $t('leadstory.' + gameParsed.gameType) }}
       </p>
       <p class="text-dark-blue flex mt-4 whitespace-pre-line">
-        <img class=" w-5 h-5 mr-3" src="/icon/circle-info.svg" />
-        {{ $t("explanation." + gameParsed.gameType) }}
+        <img class="w-5 h-5 mr-3" src="/icon/circle-info.svg" />
+        {{ $t('explanation.' + gameParsed.gameType) }}
       </p>
-      <p class="text-dark-blue my-5">
-        <span class="font-bold ">{{ $t("rulestitle") }}</span>
-      <ul>
-        <li v-for="(rule, index) in rules" :key="index">
-          {{ rule }}
-        </li>
-      </ul>
-      </p>
+      <div class="mt-3 mb-1">
+        <span class="font-bold">{{ $t('rulestitle') }}</span>
+        <p class="text-dark-blue whitespace-pre-wrap">
+          {{ $t('rules.' + gameParsed.gameType) }}
+        </p>
+      </div>
+      <div class="mt-3 mb-1">
+        <span class="font-bold">{{ $t('categorytitle') }}</span>
+        <p class="text-dark-blue whitespace-pre-wrap">
+          {{ $t('category.' + gameParsed.gameType) }}
+        </p>
+      </div>
+      <div class="mt-3 mb-1">
+        <span class="font-bold">{{ $t('pointstitle') }}</span>
+        <p class="text-dark-blue whitespace-pre-wrap">
+          {{ $t('pointexplenation.' + gameParsed.gameType) }}
+        </p>
+      </div>
     </div>
 
     <section>
-      <h2 class="text-white mt-10 mb-3">{{ $t("yourmatch") }}</h2>
-      <MatchListItem v-if="match" :key="match.id" :gameType="getGameById(match.gameId)?.gameType"
-        :gameAddOn="match.addOn" :team1="match.team1" :team2="match.team2" :team1result="match.team1Result"
-        :team2result="match.team2Result" :start="match.start" :winner="match.winner"
-        />
+      <h2 class="text-label-1 mt-5 mb-3">{{ $t('yourmatch') }}</h2>
+      <MatchListItem
+        v-if="match"
+        :key="match.id"
+        :gameType="getGameById(match.gameId)?.gameType"
+        :gameAddOn="match.addOn"
+        :team1="match.team1"
+        :team2="match.team2"
+        :team1result="match.team1Result"
+        :team2result="match.team2Result"
+        :start="match.start"
+        :winner="match.winner"
+      />
     </section>
   </UserPageLayout>
 </template>
 
 <script>
-import UserPageLayout from "@/components/UserPageLayout.vue";
-import MatchListItem from "@/components/MatchListItem.vue";
+import UserPageLayout from '@/components/UserPageLayout.vue';
+import MatchListItem from '@/components/MatchListItem.vue';
 import { ArrowLeftIcon } from '@heroicons/vue/24/solid';
 
 export default {
-  name: "GameInfoDetail",
+  name: 'GameInfoDetail',
   props: {
     game: String,
   },
@@ -64,9 +88,11 @@ export default {
     };
   },
   created() {
-    this.$store.dispatch("getLeagueStatus");
+    this.$store.dispatch('getLeagueStatus');
     if (this.game) {
-      this.gameParsed = this.$store.state.games.find((game) => game.id == this.game);
+      this.gameParsed = this.$store.state.games.find(
+        (game) => game.id == this.game,
+      );
     } else {
       this.$router.back();
     }
@@ -83,10 +109,14 @@ export default {
   },
   computed: {
     teamStatus() {
-      return this?.leagueStatus?.status?.total?.find((score) => score.team == this.$store.state.loginData?.team);
+      return this?.leagueStatus?.status?.total?.find(
+        (score) => score.team == this.$store.state.loginData?.team,
+      );
     },
     match() {
-      return this.$store.state.matches.find((match) => match.gameId == this.gameParsed?.id);
+      return this.$store.state.matches.find(
+        (match) => match.gameId == this.gameParsed?.id,
+      );
     },
     ranking() {
       const gameType = this.gameParsed?.gameType;
@@ -139,7 +169,7 @@ nav {
 }
 
 nav h3 {
-  padding: 0 .75em;
+  padding: 0 0.75em;
 }
 
 .leadstory {
@@ -160,7 +190,7 @@ nav h3 {
   margin: 1.5em 0;
 }
 
-.description>img {
+.description > img {
   width: 24px;
 }
 
@@ -168,7 +198,7 @@ header h2 {
   display: flex;
   align-items: center;
   gap: 0.5em;
-  margin: .5em 0;
+  margin: 0.5em 0;
 }
 
 .league-title {
@@ -184,17 +214,15 @@ header h2 {
   width: 100%;
 }
 
-
 .league-title-text {
   font-size: 0.85em;
   color: var(--gray-2);
   margin: 1em 0 0;
 }
 
-
 .card-currentTeam {
   color: var(--dark);
-  padding: .1em 1em;
+  padding: 0.1em 1em;
   box-shadow: 2px 2px;
   background-color: var(--yellow);
 }
