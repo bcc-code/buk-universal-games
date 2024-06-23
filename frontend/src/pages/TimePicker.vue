142f6076e191d38525c91b83bd5c764a4fb29fff
prompemannen <sschoeler99@gmail.com>
2024-06-23 11:10:05 +0200

    Styling for time picker and add cheats

--- frontend/src/pages/TimePicker.vue
+++ frontend/src/pages/TimePicker.vue
@@ -1,25 +1,50 @@
 <template>
-  <form @submit.prevent="submitForm">
-    <div
-    class="w-64"
-    >
-    <VueDatePicker 
-    v-model="date"
-    required
-      auto-apply
-      enable-seconds
-      time-picker
-      no-hours-overlay
-      :min-time="{hours:0, minutes:0, seconds:1}"
-      :max-time="{hours:0, minutes:10, seconds:0}"
-      :start-time="{hours:0, minutes:0, seconds:1}"
-      minutes-grid-increment="1" 
-      format="mm:ss"
-      :clearable="false"
-      ></VueDatePicker>
-    </div>
-      <button type="submit">Submit</button>
+  <div class="p-4 bg-white rounded-md">
+
+    <form @submit.prevent="submitForm">
+      <div class="mb-4">
+
+        Henge-tid:
+        <div
+        class="w-64  inline-block   shadow-md"
+        >
+        <VueDatePicker 
+        v-model="date"
+        required
+        auto-apply
+        enable-seconds
+        time-picker
+        no-hours-overlay
+        :min-time="{hours:0, minutes:0, seconds:1}"
+        :max-time="{hours:0, minutes:10, seconds:0}"
+        :start-time="{hours:0, minutes:0, seconds:1}"
+        minutes-grid-increment="1" 
+        format="mm:ss"
+        :clearable="false"
+        :ui="{input:'h-14 inline'}"
+        placeholder="Henge-tid"
+        ></VueDatePicker>
+      </div>
+      </div>
+      <div class="mb-4">
+        
+        Juks:
+        <input
+              class="shadow-md border-solid border-2 border-slate-200 rounded-md"
+              type="number"
+              v-model="cheats"
+              :placeholder="'Antall juks'"
+              required
+              name="reg" id="reg" 
+            />
+
+      </div>
+      <button type="submit" class="btn btn-success btn-blank h-14 p-4 shadow-md"
+      :disabled="isPending"
+      
+      >Lagre</button>
   </form>
+</div>
 </template>
 
 <script setup>
@@ -28,8 +53,10 @@ import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
 import z from 'zod'
 import { toRaw } from 'vue';
+import { useConfirmTeamResult } from '@/hooks/hooks';
 
 const date = ref();
+const cheats= ref("");
 
 const timeSchema = z.object({
   hours: z.number(),
@@ -42,11 +69,16 @@ const validatedDate = computed(()=>{
 })
 
 watch(validatedDate,(val)=>console.log(toRaw(val) ),{immediate:true})
+watch(cheats,(val)=>console.log(toRaw(val),typeof val ),{immediate:true})
 
 const submitForm = () => {
   alert('Form submitted');
   console.log(validatedDate.value)
+  confirmResult({matchId:matchId, result:result, teamId:teamId},{})
 }
+
+const { mutate: confirmResult, isPending } = useConfirmTeamResult();
+
 </script>
 
 <style>
