8cf957a21cc41d9899ff330b139cc382edcf8efe
Sigurd Schoeler <sigurd@finexa.no>
2024-07-13 18:09:25 +0200

    Formatting

--- frontend/src/pages/RegisterPointsLandWaterBeach.vue
+++ frontend/src/pages/RegisterPointsLandWaterBeach.vue
@@ -1,46 +1,47 @@
 <template>
-
   <div class="p-4 bg-white rounded-md">
-
     <form @submit.prevent="submitForm">
-       <div class="flex justify-evenly">
+      <div class="flex justify-evenly">
+        <div class="mb-4">
+          Poeng {{ match.team1 }}:
+          <br />
 
-         <div class="mb-4">
-           Poeng {{ match.team1 }}:
-          <br/>
-
-           <input
-           class="shadow-md border-solid border-2 border-slate-200 rounded-md w-36"
-           type="number"
-           v-model="pointsTeam1"
-          min="0"
-          max="9"
-          :placeholder="'Poeng'"
-          required
+          <input
+            class="shadow-md border-solid border-2 border-slate-200 rounded-md w-36"
+            type="number"
+            v-model="pointsTeam1"
+            min="0"
+            max="9"
+            :placeholder="'Poeng'"
+            required
           />
           <div v-if="calculatedResult">
-          Beregnet score: {{calculatedResult.team1Result}}
-        </div>
+            Beregnet score: {{ calculatedResult.team1Result }}
+          </div>
         </div>
         <div class="mb-4">
           Poeng {{ match.team2 }}:
-          <br/>
+          <br />
           <input
-          class="shadow-md border-solid border-2 border-slate-200 rounded-md w-36"
-          type="number"
-          v-model="pointsTeam2"
-          min="0"
-          max="9"
-          :placeholder="'Poeng'"
-          required
+            class="shadow-md border-solid border-2 border-slate-200 rounded-md w-36"
+            type="number"
+            v-model="pointsTeam2"
+            min="0"
+            max="9"
+            :placeholder="'Poeng'"
+            required
           />
           <div v-if="calculatedResult">
-            Beregnet score: {{calculatedResult.team2Result}}
+            Beregnet score: {{ calculatedResult.team2Result }}
           </div>
         </div>
       </div>
-        
-      <button type="submit" class="btn btn-success btn-blank h-14 p-4 shadow-md" :disabled="isPending">
+
+      <button
+        type="submit"
+        class="btn btn-success btn-blank h-14 p-4 shadow-md"
+        :disabled="isPending"
+      >
         Lagre
       </button>
     </form>
@@ -67,24 +68,38 @@ const props = defineProps<{
 const pointsTeam1 = ref<number | ''>('');
 const pointsTeam2 = ref<number | ''>('');
 
-const calculatedResult = computed<{ team1Result: number; team2Result: number } | undefined>(() => {
+const calculatedResult = computed<
+  { team1Result: number; team2Result: number } | undefined
+>(() => {
   const validatedPointsTeam1 = pointsTeam1.value;
   const validatedPointsTeam2 = pointsTeam2.value;
-  if (!(typeof validatedPointsTeam1 === "number")) return;
-  if (!(typeof validatedPointsTeam2 === "number")) return;
+  if (!(typeof validatedPointsTeam1 === 'number')) return;
+  if (!(typeof validatedPointsTeam2 === 'number')) return;
 
   const minScore = 1;
   const maxScore = 20;
   const minPoints = 0;
   const maxPoints = 9;
 
-    const team1Result = lerp(minPoints, maxPoints, minScore, maxScore, validatedPointsTeam1);
-    const team2Result = lerp(minPoints, maxPoints, minScore, maxScore, validatedPointsTeam2);
+  const team1Result = lerp(
+    minPoints,
+    maxPoints,
+    minScore,
+    maxScore,
+    validatedPointsTeam1,
+  );
+  const team2Result = lerp(
+    minPoints,
+    maxPoints,
+    minScore,
+    maxScore,
+    validatedPointsTeam2,
+  );
 
-    return {
-      team1Result: floatToInt(team1Result),
-      team2Result: floatToInt(team2Result),
-    };
+  return {
+    team1Result: floatToInt(team1Result),
+    team2Result: floatToInt(team2Result),
+  };
 });
 
 const showSuccess = ref<boolean>(false);
@@ -97,14 +112,25 @@ const showSuccessToast = () => {
 };
 
 // 🧹 create a new endpoint which both of these, or at least create a hook so we know what interface we want.
-const { mutate: confirmResultTeam1, isPending: isPendingTeam1, error: errorTeam1 } = useConfirmTeamResult();
-const { mutate: confirmResultTeam2, isPending: isPendingTeam2, error: errorTeam2 } = useConfirmTeamResult();
+const {
+  mutate: confirmResultTeam1,
+  isPending: isPendingTeam1,
+  error: errorTeam1,
+} = useConfirmTeamResult();
+const {
+  mutate: confirmResultTeam2,
+  isPending: isPendingTeam2,
+  error: errorTeam2,
+} = useConfirmTeamResult();
 
 const error = computed(() => errorTeam1.value || errorTeam2.value);
 const isPending = computed(() => isPendingTeam1.value || isPendingTeam2.value);
 
 const submitForm = () => {
-  if (!calculatedResult.value) throw Error('CalculatedResult returned undefined. The form is invalid even if it can be submitted. You might be missing some validation on the form fields.');
+  if (!calculatedResult.value)
+    throw Error(
+      'CalculatedResult returned undefined. The form is invalid even if it can be submitted. You might be missing some validation on the form fields.',
+    );
 
   const matchId = props.match.matchId;
 
@@ -114,8 +140,16 @@ const submitForm = () => {
   const team1Id = props.match.team1Id;
   const team2Id = props.match.team2Id;
 
-  const variablesTeam1 = { matchId: matchId, result: resultTeam1, teamId: team1Id };
-  const variablesTeam2 = { matchId: matchId, result: resultTeam2, teamId: team2Id };
+  const variablesTeam1 = {
+    matchId: matchId,
+    result: resultTeam1,
+    teamId: team1Id,
+  };
+  const variablesTeam2 = {
+    matchId: matchId,
+    result: resultTeam2,
+    teamId: team2Id,
+  };
 
   confirmResultTeam1(variablesTeam1, {
     onSuccess() {
