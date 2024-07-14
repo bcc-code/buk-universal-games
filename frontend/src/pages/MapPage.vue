68da2f4caa9550b79e40801dd7ec763a8257f540
Sigurd Schoeler <sigurd@finexa.no>
2024-07-14 14:44:13 +0200

    Make image into link which opens image in new tab

--- frontend/src/pages/MapPage.vue
+++ frontend/src/pages/MapPage.vue
@@ -1,53 +1,25 @@
 <template>
   <div class="root">
-    <div ref="mapWrapper" class="map-wrapper">
+    <a :href="map" target="_blank">
       <img ref="mapImage" :src="map" alt="map" />
-    </div>
+    </a>
     <UserMenu />
   </div>
 </template>
 <script setup lang="ts">
-import { ref, computed, onMounted } from 'vue';
-import PinchZoom from 'pinch-zoom-js';
-import UserMenu from '@/components/UserMenu.vue';
-import { useStore } from 'vuex';
+import { computed } from 'vue';
+import UserMenu from '../components/UserMenu.vue';
+import { useSigninResponse } from '../hooks/hooks';
 
-const store = useStore();
+const { data } = useSigninResponse();
 
-const mapWrapper = ref<HTMLElement | null>(null);
-const mapImage = ref<HTMLImageElement | null>(null);
-const map = ref<string | null>(null);
-
-const league = computed(() => {
-  return store.state.loginData.league?.substring(0, 1).toLowerCase();
+const leagueId = computed(() => {
+  return data.value?.leagueId;
 });
 
-const initializeMap = () => {
-  if (league.value === 'k') {
-    map.value = '/image/ubg-beach-small.png';
-  } else {
-    map.value = '/image/ubg-arena-small.png';
-  }
-};
-
-onMounted(() => {
-  initializeMap();
-  mapWrapper.value = document.querySelector(
-    '.map-wrapper',
-  ) as HTMLElement | null;
-  mapImage.value = document.querySelector(
-    '.map-wrapper img',
-  ) as HTMLImageElement | null;
-
-  if (mapWrapper.value) {
-    new PinchZoom(mapWrapper.value, {
-      minZoom: 1,
-      maxZoom: 10,
-      animationDuration: 150,
-      tapZoomFactor: 3,
-      draggableUnzoomed: true,
-    });
-  }
+const map = computed(() => {
+  const localLeagueId = leagueId.value;
+  return localLeagueId ? `/image/Sone ${localLeagueId}.jpg` : undefined;
 });
 </script>
 <style scoped>
@@ -56,20 +28,9 @@ onMounted(() => {
   height: 100%;
 }
 
-.map-wrapper {
+.root img {
   width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #5fa46e;
-  background-image: linear-gradient(135deg, #5fa46e, #118144);
-}
-
-.map-wrapper img {
-  height: 100%;
-  width: 100%;
+  height: auto;
   object-fit: contain;
-  display: block;
 }
 </style>
