29b065762ee64734532aebf4b03e3aea0181984a
Sigurd Schoeler <sigurd@finexa.no>
2024-07-14 14:15:26 +0200

    Add pinch zoom for map

--- frontend/src/pages/MapPage.vue
+++ frontend/src/pages/MapPage.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="root">
-    <div ref="map" class="map-wrapper relative">
-      <img :src="map" alt="" />
-    </div>
+    <img ref="mapImage" :src="map" alt="map" />
     <UserMenu />
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, onMounted } from 'vue';
+import PinchZoom from 'pinch-zoom-js';
 import UserMenu from '../components/UserMenu.vue';
 import { useSigninResponse } from '../hooks/hooks';
 
@@ -23,36 +22,26 @@ const map = computed(() => {
   return localLeagueId ? `/image/Sone ${localLeagueId}.jpg` : undefined;
 });
 
-// import PinchZoom from 'pinch-zoom-js'
+onMounted(() => {
+  const mapImage = document.querySelector(
+    '.map-wrapper img',
+  ) as HTMLImageElement | null;
 
-//   new PinchZoom(this.$refs.map, {
-//     minZoom: 1,
-//     maxZoom: 10,
-//     animationDuration: 150,
-//     tapZoomFactor: 3,
-//     draggableUnzoomed: true
-//   })
+  if (mapImage) {
+    new PinchZoom(mapImage, {
+      minZoom: 1,
+      maxZoom: 10,
+      animationDuration: 150,
+      tapZoomFactor: 3,
+      draggableUnzoomed: true,
+    });
+  }
+});
 </script>
 
 <style scoped>
 .root {
   width: 100%;
   height: 100%;
-}
-
-.map-wrapper {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #5fa46e;
-  background-image: linear-gradient(135deg, #5fa46e, #118144);
-}
-
-.map-wrapper img {
-  height: 100%;
-  width: 100%;
-  object-fit: contain;
 }
 </style>
