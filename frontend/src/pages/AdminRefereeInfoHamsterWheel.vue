dde6a69d0dc47c683dab95ad13264b7138bb76c1
Sigurd Schoeler <sschoeler99@gmail.com>
2024-07-15 14:06:58 +0200

    Add rule to manuscript about stepping outside

--- frontend/src/pages/AdminRefereeInfoHamsterWheel.vue
+++ frontend/src/pages/AdminRefereeInfoHamsterWheel.vue
@@ -11,15 +11,16 @@
 			<li>Såfremt laget har nok deltakere kan personene i hjulet byttes ut med 14 nye personer når laget er halvveis.</li>
 			<li>Laget må over streken med hele hjulet før laget kan bytte deltakerne.</li>
 			<li>Det laget som er først tilbake med hele laget over startstreken vinner. Tiden vil også bli registrert, så her gjelder det for begge lag å gjøre det raskest mulig selv om en ikke kommer først i mål.</li>
+			<li>Laget må holde seg innenfor teppet gjennom hele løypen. Hver fot som treffer bakken utenfor teppet gir tilleggstid. Dette gjelder også dersom duken ikke er over hodet på deltakerne.</li>
 			<li>Dere får 2 minutter på å gjøre dere klare.</li>
 			<li>Oppgaven begynner på mitt (dommerens) signal.</li>
 			<li>Er oppgaven forstått?</li>
 			<li>Da kan lagene gjøre seg klare ved startstreken. Gruppen som skal ta over kan stelle seg på andre siden av banen. Nedtelling på 2 minutter starter nå.</li>
 			<li>Tiden er nå ute. Er alle lag klare?</li>
 		</ul>
-		</div>
+	</div>
 
-		<div class="rounded-md p-4 mb-4 bg-blue-50">
+	<div class="rounded-md p-4 mb-4 bg-blue-50">
 		<span class="font-bold">Til dommer:</span>
 		<ul class="list-disc list-inside">
 			<li>To dommere plasseres på hver sin side av lagene og registrerer på telefon hvor ofte lagene tråkker utenfor båndet og hver gang laget ikke har stoffet over hodet.</li>
@@ -32,9 +33,9 @@
 			<li>Rett opp startstrek og andre gjerder om nødvendig.</li>
 			<li>Gjør klar for ny gruppe!</li>
 		</ul>
-		</div>
+	</div>
 
-		<div class="rounded-md p-4 mb-4 bg-red-50">
+	<div class="rounded-md p-4 mb-4 bg-red-50">
 		<span class="font-bold">Helse, miljø og sikkerhet:</span>
 		<ul class="list-disc list-inside">
 			<li>Vær oppmerksom på at hamsterhjulet innebærer at mange personer skal gå i et høyt tempo samtidig og tett på hverandre som kan medføre at noen kan falle. Dette anses som en middels risiko.</li>
