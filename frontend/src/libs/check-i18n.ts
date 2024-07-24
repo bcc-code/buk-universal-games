c7bbc15b61e0772d295df40aeb4fc9a174d8cdb2
Zikoat <sschoeler99@gmail.com>
2024-07-24 15:16:55 +0200

    Remove finnish translation

--- frontend/src/libs/check-i18n.ts
+++ frontend/src/libs/check-i18n.ts
@@ -4,7 +4,6 @@ import * as de from '../locales/de.json';
 import * as es from '../locales/es.json';
 import * as nl from '../locales/nl.json';
 import * as pl from '../locales/pl.json';
-import * as fi from '../locales/fi.json';
 import * as fr from '../locales/fr.json';
 import * as ua from '../locales/ua.json';
 
@@ -13,7 +12,6 @@ const _check_de: typeof nb = de;
 const _check_es: typeof nb = es;
 const _check_nl: typeof nb = nl;
 const _check_pl: typeof nb = pl;
-const _check_fi: typeof nb = fi;
 const _check_fr: typeof nb = fr;
 const _check_ua: typeof nb = ua;
 
@@ -22,7 +20,6 @@ const _check_nb_de: typeof de = nb;
 const _check_nb_es: typeof es = nb;
 const _check_nb_nl: typeof nl = nb;
 const _check_nb_pl: typeof pl = nb;
-const _check_nb_fi: typeof fi = nb;
 const _check_nb_fr: typeof fr = nb;
 const _check_nb_ua: typeof ua = nb;
 
@@ -34,7 +31,6 @@ removeUnusedWarning(
   _check_es,
   _check_nl,
   _check_pl,
-  _check_fi,
   _check_fr,
   _check_ua,
   _check_nb_en,
@@ -42,7 +38,6 @@ removeUnusedWarning(
   _check_nb_es,
   _check_nb_nl,
   _check_nb_pl,
-  _check_nb_fi,
   _check_nb_fr,
   _check_nb_ua,
 );
