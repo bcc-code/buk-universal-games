873a214e15388db9df9d616ebcd8ad9b5604cf07
Zikoat <sschoeler99@gmail.com>
2024-07-24 15:14:43 +0200

    Fix internationalisation type-check

--- frontend/src/libs/check-i18n.ts
+++ frontend/src/libs/check-i18n.ts
@@ -4,14 +4,8 @@ import * as de from '../locales/de.json';
 import * as es from '../locales/es.json';
 import * as nl from '../locales/nl.json';
 import * as pl from '../locales/pl.json';
-import * as cn from '../locales/cn.json';
 import * as fi from '../locales/fi.json';
 import * as fr from '../locales/fr.json';
-import * as hu from '../locales/hu.json';
-import * as it from '../locales/it.json';
-import * as ro from '../locales/ro.json';
-import * as ru from '../locales/ru.json';
-import * as tr from '../locales/tr.json';
 import * as ua from '../locales/ua.json';
 
 const _check_en: typeof nb = en;
@@ -19,14 +13,8 @@ const _check_de: typeof nb = de;
 const _check_es: typeof nb = es;
 const _check_nl: typeof nb = nl;
 const _check_pl: typeof nb = pl;
-const _check_cn: typeof nb = cn;
 const _check_fi: typeof nb = fi;
 const _check_fr: typeof nb = fr;
-const _check_hu: typeof nb = hu;
-const _check_it: typeof nb = it;
-const _check_ro: typeof nb = ro;
-const _check_ru: typeof nb = ru;
-const _check_tr: typeof nb = tr;
 const _check_ua: typeof nb = ua;
 
 const _check_nb_en: typeof en = nb;
@@ -34,14 +22,8 @@ const _check_nb_de: typeof de = nb;
 const _check_nb_es: typeof es = nb;
 const _check_nb_nl: typeof nl = nb;
 const _check_nb_pl: typeof pl = nb;
-const _check_nb_cn: typeof cn = nb;
 const _check_nb_fi: typeof fi = nb;
 const _check_nb_fr: typeof fr = nb;
-const _check_nb_hu: typeof hu = nb;
-const _check_nb_it: typeof it = nb;
-const _check_nb_ro: typeof ro = nb;
-const _check_nb_ru: typeof ru = nb;
-const _check_nb_tr: typeof tr = nb;
 const _check_nb_ua: typeof ua = nb;
 
 function removeUnusedWarning(...args: unknown[]): void {}
@@ -52,27 +34,15 @@ removeUnusedWarning(
   _check_es,
   _check_nl,
   _check_pl,
-  _check_cn,
   _check_fi,
   _check_fr,
-  _check_hu,
-  _check_it,
-  _check_ro,
-  _check_ru,
-  _check_tr,
   _check_ua,
   _check_nb_en,
   _check_nb_de,
   _check_nb_es,
   _check_nb_nl,
   _check_nb_pl,
-  _check_nb_cn,
   _check_nb_fi,
   _check_nb_fr,
-  _check_nb_hu,
-  _check_nb_it,
-  _check_nb_ro,
-  _check_nb_ru,
-  _check_nb_tr,
   _check_nb_ua,
 );
